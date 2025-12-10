90a7339e88b94bb7bd72c16c8613cccee63582a2
KaceObeng <eaobeng118@st.ug.edu.gh>
2025-12-10 11:37:59 -0500

    my assignmentr

---
+++ Basics of python.py
@@ -0,0 +1,25 @@
+#1
+playlist =["Why love","Sacrifice","Rebel Music","Kelebu","Whos Dat Girl"]
+print(playlist) 
+
+#2
+top_artiste=("Black Sherif","Rema","Ayra Starr","Lil Baby")
+print(top_artiste)
+
+#3
+my_music=[playlist,top_artiste]
+print(my_music)
+
+#4
+print(len(my_music))
+print(playlist[2])
+
+#6
+playlist[0]="Perfect"
+print(playlist)
+
+#7
+playlist.append("Blinding Lights")
+
+#8
+print(playlist)
